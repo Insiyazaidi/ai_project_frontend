ef3f784cff11adcadf7312c19ee07c9c86802f58
Your Name <you@example.com>
2026-01-20 23:46:29 +0530

    final for 20 jan

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,14 @@ import { BrowserRouter as Router , Routes , Route , Navigate } from "react-route
 import Loginpage from "./pages/auth/Loginpage";
 import Registerpage from "./pages/auth/Registerpage";
 import Notfoundpage from "./pages/Notfoundpage";
+import Dashboardpage from "./pages/dashboard/Dashboardpage";
+import Quiztakepage from "./pages/Quizzes/Quiztakepage";
+import Quizresultpage from "./pages/Quizzes/Quizresultpage";
+import Documentdetailpage from "./pages/Documents/Documentdetailpage";
+import Documentlistpage from "./pages/Documents/Documentlistpage";
+import Flashcardlistpage from "./pages/Flashcards/Flashcardlistpage";
+import Flashcardpage from "./pages/Flashcards/Flashcardpage";
+import Profilepage from "./pages/Profile/Profilepage";
 const App=()=>{
   const isAuthenticated = false ; 
   const loading = false; 
@@ -19,6 +27,14 @@ const App=()=>{
     <Route path="/login" element={<Loginpage/>}/>
     <Route path="/register" element={<Registerpage/>}/>
     <Route path="*" element={<Notfoundpage/>}/>
+    <Route path="/dashboard" element={<Dashboardpage/>}/>
+     <Route path="/documents/:id" element={<Documentdetailpage/>}/>
+    <Route path="/documents" element={<Documentlistpage/>}/>
+    <Route path="/flashcards" element={<Flashcardlistpage/>}/>
+     <Route path="/documents/:id/flashcards" element={<Flashcardpage/>}/>
+    <Route path="/quizzes/:quizId" element={<Quiztakepage/>}/>
+    <Route path="/quizzes/:quizId/results" element={<Quizresultpage/>}/>
+    <Route path="/profile" element={<Profilepage/>}/>
   </Routes>
 </Router>
   )
