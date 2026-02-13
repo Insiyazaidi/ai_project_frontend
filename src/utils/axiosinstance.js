6dabc9f09996abeb0f0c33047f4eb9729bdc7afd
Your Name <zaidiinsiya83@gmail.com>
2026-02-13 22:24:13 +0530

    axios instances

--- src/utils/axiosinstance.js
+++ src/utils/axiosinstance.js
@@ -1,24 +1,26 @@
 import axios from "axios"
 import {BASE_URL} from "./apipath"
-const axiosinstance = axios.create({
-    baseUrl : BASE_URL,
-    timeout:8000,
+const axiosinstance = axios.create({   // think it like a custom axios with default settings made by me 
+    baseURL : BASE_URL,
+    timeout:8000,  // if we fail to get res withing 8 sec then req will fail
     headers:{
-        "Content-Type":"application/json",
+        "Content-Type":"application/json", //  I am sending JSON data & want JSON response These headers
+        //  will automatically be added to every request
         Accept:"application/json"
     }
 })
-// request interceptor 
-axiosinstance.interceptors.request.use(
-    (config)=>{
+
+// request interceptor , interceptor is a function 
+axiosinstance.interceptors.request.use(  // this is just adding token from localstorage to req  
+    (config)=>{  // config contain everything abt req like url , data , headers , methods 
         const accesstoken = localStorage.getItem("token")
         if(accesstoken){
             config.headers.Authorization=`Bearer ${accesstoken}`
         }
         return config
     },
-    (error)=>{
-        return Promise.reject(error)
+    (error)=>{  // axios return promise 
+        return Promise.reject(error)  // pass this error to .catch()-- 
     }
 )
 // respnose interceptor
