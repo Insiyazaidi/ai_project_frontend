2c47738b70b6e3f643a3f0a7a4343d25f6aa7f21
Your Name <zaidiinsiya83@gmail.com>
2026-02-14 01:04:21 +0530

    defined url

--- src/services/progressservice.js
+++ src/services/progressservice.js
@@ -1,2 +1,13 @@
 import axiosinstance from "../utils/axiosinstance.js";
 import { API_PATHS } from "../utils/apipath.js";
+
+const getdashboarddata = async()=>{ 
+    try {
+     const response = await axiosinstance.get(API_PATHS.PROGRESS.GET_DASHBOARD)
+     return response.data   
+    } catch (error) {
+       throw error.response?.data|| {message:"Failed to fetch quiz"} 
+    }
+}
+const progressservice = {getdashboarddata}
+export default progressservice
