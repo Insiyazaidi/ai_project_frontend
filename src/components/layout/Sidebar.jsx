f7d3e0d72b75201d013681a3b5eea966980c6d4f
Your Name <zaidiinsiya83@gmail.com>
2026-02-20 21:24:17 +0530

    just comment

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { NavLink , useNavigate } from 'react-router'
 import {useAuth} from "../../context/Authcontext"
 import { LayoutDashboard , FileText , User , LogOut , BrainCircuit , BookOpen , X } from 'lucide-react'
-const Sidebar = ({ issidebaropen , togglesidebar}) => { {/* taken as parameter  ...  */}
+const Sidebar = ({ issidebaropen , togglesidebar}) => { {/* taken as paramter .need to start from this  ...  */}
 
   const {logout } = useAuth()
   const navigate = useNavigate()
