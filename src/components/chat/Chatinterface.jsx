4f5ea31699638e2e5253d886d29adb7c84c41510
Your Name <zaidiinsiya83@gmail.com>
2026-03-08 23:45:26 +0530

    8 mar

--- src/components/chat/Chatinterface.jsx
+++ src/components/chat/Chatinterface.jsx
@@ -127,7 +127,7 @@ if(initialloading){
 history.map(rendermessage)
 
  )}
-  <div ref={messageendref}></div>   {/* for scrolling*/} 
+  <div ref={messageendref}></div>   {/* for scrolling waiting */} 
 
 
 {/* for 3 dots while loading ... */}  
