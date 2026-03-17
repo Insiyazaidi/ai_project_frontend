e1274075433cefffcd84ef1f427be3cca27b4cd3
Your Name <zaidiinsiya83@gmail.com>
2026-03-18 00:55:45 +0530

    18 march

--- src/components/flashcards/Flashcardmanager.jsx
+++ src/components/flashcards/Flashcardmanager.jsx
@@ -10,6 +10,14 @@ import Modal from '../common/Modal'
 import Flashcard from './Flashcard'
 const Flashcardmanager = ({documentid}) => {
 
+    const [flashcards , setflashcards] = useState([])
+    const [selectedset , setselectedset] = useState(null)
+    const [loading , setloading]= useState(true)
+    const [generating , setgenerating ]= useState(false)
+    const [currentcardindex , setcurrentcardindex] = useState(0)
+        const [isdeletemodalopen , setisdeletemodalopen ]= useState(false)
+    const [deleting , setdeleting ]= useState(false)
+    const [settodelete , setsettodelete] = useState(null)
     
 
   return (
