967d53cc3d66e32d9e4b8e18443a14ccacef8398
Your Name <zaidiinsiya83@gmail.com>
2026-03-19 06:00:09 +0530

    19 mar

--- src/components/flashcards/Flashcardmanager.jsx
+++ src/components/flashcards/Flashcardmanager.jsx
@@ -199,9 +199,29 @@ return (
 }
 
   return (
-    <div className='bg-white/80 backdrop-blur-xl border border-slate-200 rounded-3xl shadow-xl shadow-slate-200/80 p-8'>
+
+    <>
+
+       <div className='bg-white/80 backdrop-blur-xl border border-slate-200 rounded-3xl shadow-xl shadow-slate-200/80 p-8'>
  {selectedset ? renderflashcardviewer():rendersetlist()}
     </div>
+
+ {/* Delete configuration model */}
+
+<Modal isOpen={isdeletemodalopen} onClose={()=>setisdeletemodalopen(false)} title="Delete Flashcard Set?">
+<div className='space-y-6'>
+<p className='text-sm  text-slate-600'>Are you sure you want to delete this flashcard set? This action cannot be undone and all cards will be permanently removed </p>
+<div className='flex items-center justify-end gap-3 pt-2'>
+    <button type='button'onClick={()=>setisdeletemodalopen(false)} className='px-5 h-11 bg-slate-100 hover:bg-slate-200 text-slate-700 font-medium text-sm rounded-xl transition-all duration-200 disabled:opacity-50 disabled:cursor-not-allowed'>Cancel</button>
+    <button onClick={handleconfirmdelete} disabled={deleting} className=' px-5 h-11 bg-linear-to-r from-rose-500 to-red-500 hover:from-rose-600 hover:to-red-600 text-white font-semibold text-sm rounded-xl transition-all duration-200 active:scale-95 disabled:opacity-50 disabled:cursor-not-allowed disabled:active:scale-100 ' >{deleting? (<span className='flex items-center gap-2' ><div className='w-4 h-4  border-2 border-white/30 rounded-full animate-spin'>Deleting</div></span>):"Delete Set"}</button>
+</div>
+
+</div>
+    </Modal>
+
+ 
+    </>
+ 
   )
 }
 
