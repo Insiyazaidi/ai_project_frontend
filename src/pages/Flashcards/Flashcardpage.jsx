50ac528d1173d4236e6737f089deaaee7b027229
Your Name <zaidiinsiya83@gmail.com>
2026-03-29 22:05:47 +0530

    29

--- src/pages/Flashcards/Flashcardpage.jsx
+++ src/pages/Flashcards/Flashcardpage.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
-
+import { useParams , Link } from 'react-router-dom'
+import {ArrowLeft , Plus , ChevronLeft , ChevronRight , Trash2} from "lucide-react"
+import toast from "react-hot-toast"
+import flashcardservice from '../../services/flashcardservice.js'
+import Emptystate from '../../components/common/Emptystate.jsx'
+import Pageheader from '../../components/common/Pageheader.jsx'
+import aiservice from "../../services/aiservice.js"
+import Spinner from '../../components/common/Spinner.jsx'
+import Modal from '../../components/common/Modal.jsx'
+import Flashcard from '../../components/flashcards/Flashcard.jsx'
 const Flashcardpage = () => {
   return (
     <div>Flashcardpage</div>
