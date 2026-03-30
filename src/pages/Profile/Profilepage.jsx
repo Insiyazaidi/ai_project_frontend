d481ca07a3a095d0608c5c7936c140043a3e5338
Your Name <zaidiinsiya83@gmail.com>
2026-03-31 01:25:53 +0530

    31 marchh..

--- src/pages/Profile/Profilepage.jsx
+++ src/pages/Profile/Profilepage.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useAuth } from '../../context/Authcontext'
 import toast from 'react-hot-toast'
 import  {User , Mail , Lock} from "lucide-react"
 import Pageheader from '../../components/common/Pageheader'
+import Button from '../../components/common/Button'
+import authservice from '../../services/authservice'
 const Profilepage = () => {
   const [loading , setloading]=useState(true)
   const [passwordloading , setpasswordloading] = useState(false)
@@ -11,6 +13,57 @@ const Profilepage = () => {
       const [currentpassword , setcurrentpassword] = useState("")
     const [newpassword , setnewpassword] = useState("")
     const [confirmnewpassword , setconfirmnewpassword] = useState("")
+    
+    useEffect(()=>{
+   
+         const fetchprofile = async()=>{
+
+   try {
+
+        const {data} = await authservice.getprofile()
+        setusername(data.username)
+        setemail(data.email)
+      
+   }
+       catch (error) {
+        toast.error("failed to fetch profile data")
+        console.error(error)
+      }
+      finally{
+        setloading(false)
+      }
+    }
+
+    fetchprofile()
+    } , [])
+
+
+const handlechangepassword = async()=>{
+  e.preventDefault()
+  if(newpassword !==confirmnewpassword){
+    toast.error("New passwords do not match")
+    return
+  }
+  if(newpassword <6){
+    toast.error("New password must be at least 6 characters long")
+    return
+  }
+  setpasswordloading(true)
+  try {
+    await authservice.changepassword({currentpassword , newpassword})
+    toast.success("Password changed successfully")
+    setcurrentpassword("")
+    setconfirmnewpassword("")
+    setnewpassword("")
+  } catch (error) {
+    toast.error(error.message || "Failed to change password")
+  }
+  finally{
+    setpasswordloading(false)
+  }
+}
+
+
   return (
     <div>
       <Pageheader title="Profile Settings"/>
@@ -32,12 +85,64 @@ const Profilepage = () => {
 <div className='relative'>
   <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
      <Mail className='h-4 w-4 text-neutral-400'/>
-   <p className='w-full h-9 pl-9 pr-3 pt-2 border border-neutral-200 rounded-lg bg-neutral-50 text-sm text-neutral-900 '>{email}</p> 
+  
     </div>
+     <p className='w-full h-9 pl-9 pr-3 pt-2 border border-neutral-200 rounded-lg bg-neutral-50 text-sm text-neutral-900 '>{email}</p> 
 </div>
 </div>
     </div>
 
+{/*  Change password form*/}
+
+<div className='bg-white border border-neutral-200 rounded-lg p-6'>
+  <h3 className='text-lg font-semibold text-neutral-900 mb-4'>Change Password</h3>
+  <form onSubmit={handlechangepassword} className=''>
+
+<div>
+
+
+  <label className='block text-xs font-medium text-neutral-700 mb-1.5'>Current Password</label>
+  <div className='relative'>
+    <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
+      <Lock className='h-4 w-4 text-neutral-400'/>
+    </div>
+ <input type='password'value={currentpassword} onChange={(e)=>setcurrentpassword(e.target.value)} required className='w-full h-9 pl-9 pr-3 border-neutral-200 rounded-lg bg-white text-sm text-neutral-900 placeholder-neutral-400 tansition-colors duration-150 focus:ring-2 focus:ring-primary-dark focus:border-transparent '></input>
+  </div>
+</div>
+
+<div>
+
+<label className='block text-xs font-medium text-neutral-700 mb-1.5'>New Password</label>
+<div className='relative'>
+  <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
+    <Lock className='h-4 w-4 text-neutral-400'/>
+  </div>
+  <input className='w-full h-9 pl-9 pr-3 border-neutral-200 rounded-lg bg-white text-sm text-neutral-900 placeholder-neutral-400 tansition-colors duration-150 focus:ring-2 focus:ring-primary-dark focus:border-transparent' onChange={(e)=>setnewpassword(e.target.value)} value={newpassword} type="password"/>
+</div>
+</div>
+
+
+<div>
+
+<label className='block text-xs font-medium text-neutral-700 mb-1.5'> Confirm New Password</label>
+<div className='relative'>
+  <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
+    <Lock className='h-4 w-4 text-neutral-400'/>
+  </div>
+  <input className='w-full h-9 pl-9 pr-3 border-neutral-200 rounded-lg bg-white text-sm text-neutral-900 placeholder-neutral-400 tansition-colors duration-150 focus:ring-2 focus:ring-primary-dark focus:border-transparent' onChange={(e)=>setconfirmnewpassword(e.target.value)} value={confirmnewpasswordpassword} type="password"/>
+</div>
+</div>
+
+<div>
+  <Button type='submit' disabled={passwordloading}>
+    {passwordloading ? "Changing" : "Change Password"}
+  </Button>
+</div>
+
+
+
+  </form>
+</div>
 
 
 
