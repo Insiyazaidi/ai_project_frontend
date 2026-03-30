ba1762f4ab57f0c59046eb1f6790b03e3622d2a9
Your Name <zaidiinsiya83@gmail.com>
2026-03-30 23:09:10 +0530

    30 mar

--- src/pages/Flashcards/Flashcardpage.jsx
+++ src/pages/Flashcards/Flashcardpage.jsx
@@ -36,8 +36,96 @@ const Flashcardpage = () => {
   useEffect(()=>{
     fetchflashcard()
   } , [documentid])
+
+const handlegenerateflashcards = async()=>{
+  setgenerating(true)
+  try {
+    await aiservice.generateflashcards(documentid)
+    toast.success("Flashcards generated successfully")
+  } catch (error) {
+    toast.error(error.message || "Failed to generate flashcards")
+  }
+  finally{
+    setgenerating(false)
+  }
+}
+
+const handlenextcard = ()=>{
+    if(selectedset){
+        handlereview(currentcardindex)  // jb bhi prev ya next card pr click krrhe h toh phle handlereview call horha hh 
+        setcurrentcardindex((previndex)=>(previndex+1)%selectedset.cards.length)  // circular iteration ke liye ... 
+        // previndex -9 (last card) , previndex+1 10 %10 = 0 which means it will show 0 th index card ie first one ... 
+    }
+}
+
+
+const handleprevcard = ()=>{
+    if(selectedset){
+        handlereview(currentcardindex)
+         setcurrentcardindex((previndex)=>(previndex-1+selectedset.cards.length)%selectedset.cards.length)
+    }
+}
+
+
+const handlereview = async(index)=>{  // yha pr vo card aarha h vo hmne dekh liya .. 
+    const currentcard = selectedset?.cards[currentcardindex]  // currentcard ka data 
+    if(!currentcard) return 
+    try {
+        await flashcardservice.reviewflashcard(currentcard._id , index)
+        toast.success("Flashcard reviewed")
+    } catch (error) {
+        toast.error("failed to review flashcard")
+    }
+}
+
+const handletogglestar = async (cardid) => {
+  try {
+    await flashcardservice.togglestar(cardid);
+setflashcards((prevflashcards)=>prevflashcards.map((card)=>card._id === cardid ? {...card , isstarred:!card.isstarred} : card))
+    toast.success("Flashcard starred status updated!");
+  } catch (error) {
+    toast.error("Failed to update star status");
+  }
+};
+
+
+
+const handledeleteflashcardset = async()=>{
+setdeleting(true)
+try {
+    await flashcardservice.deleteflashcardset(flashcardsets._id)  // upr phle save krliya set jo delete krna h phir ._id use krke call kiya deletflashcard ko 
+    toast.success("Flashcard set deleted successfully")
+   setisdeletemodalopen(false)
+    fetchflashcard()
+} catch (error) {
+     toast.error(   error.message || "failed to delete flashcard")
+}
+finally{
+    setdeleting(false)
+}
+}
+
+const renderflashcardcontent =()=>{
+  if(loading){
+    return <Spinner/>
+  }
+  if(flashcards.length ===0){
+    return <Emptystate title="No flashcards yet" description="Generate flashcards from your document to start learning"/>
+  }
+}
+
+const currentcard = flashcards[currentcardindex]
+
+
+
+
   return (
-    <div>Flashcardpage</div>
+    <div>
+
+
+
+      
+    </div>
   )
 }
 
