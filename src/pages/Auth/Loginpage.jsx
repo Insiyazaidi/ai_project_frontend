c073c24d27579f6655d00848cba8c39cc344dbf3
Your Name <zaidiinsiya83@gmail.com>
2026-03-31 22:02:45 +0530

    31 frontend

--- src/pages/Auth/Loginpage.jsx
+++ src/pages/Auth/Loginpage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 import { BrainCircuit, Mail, Lock, ArrowRight, ArrowLeft } from "lucide-react";
 import { useNavigate, Link } from "react-router-dom";
 import { useAuth } from "../../context/Authcontext";
