6292279d76705a1f328440101ad093e277463423
Your Name <zaidiinsiya83@gmail.com>
2026-03-31 23:15:00 +0530

    fixing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router , Routes , Route  } from "react-router-dom"
-import Loginpage from "./pages/auth/Loginpage";
-import Registerpage from "./pages/auth/Registerpage";
+
 import Notfoundpage from "./pages/Notfoundpage";
 import Dashboardpage from "./pages/dashboard/Dashboardpage";
 import Quiztakepage from "./pages/Quizzes/Quiztakepage";
@@ -26,8 +25,7 @@ const {isAuthenticated , loading} = useAuth()
 <Router>
   <Routes>
     <Route path="/" element={<Firstimp/>}></Route>
-    <Route path="/login" element={<Loginpage/>}/>
-    <Route path="/register" element={<Registerpage/>}/>
+    
    <Route element={<Protectedroute/>}/>
     <Route path="/dashboard" element={<Dashboardpage/>}/>
      <Route path="/documents/:id" element={<Documentdetailpage/>}/>
--- src/components/auth/Protectedroute.jsx
+++ src/components/auth/Protectedroute.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import { Outlet } from 'react-router';
 import { Navigate } from 'react-router';
 import Applayout from '../layout/Applayout';
