85c702dbcb6d75c3fcea81b45c87b3d96b93e1cf
Your Name <zaidiinsiya83@gmail.com>
2026-04-06 22:45:45 +0530

    ,..

--- src/Firstimp.jsx
+++ src/Firstimp.jsx
@@ -1,82 +1,90 @@
-import React from 'react'
-import examboy from "../src/assets/Exams-brooo.svg"
+import React, { useState, useEffect } from 'react';
+import examboy from "../src/assets/Exams-brooo.svg";
 import { useNavigate } from 'react-router';
-import { motion , AnimatePresence } from "framer-motion";
-import {container , item , heading , buttonAnim , imageAnim} from "./motions.js"
+import { motion, AnimatePresence } from "framer-motion";
+import { container, item, heading, buttonAnim, imageAnim } from "./motions.js";
 import { useAuth } from './context/Authcontext.jsx';
-const Firstimp = () => {
-  const texts = [
-  "not just answers.",
-  "real understanding.",
-  "deep focus.",
-  "true clarity."
-];
-    const navigate = useNavigate()
-    const { isAuthenticated } = useAuth();
-  return (
-   <motion.div variants={container}
-  initial="hidden"
-  animate="show" className="min-h-screen bg-linear-to-r from-primary-dark to-primary text-white relative overflow-hidden">
 
-      <div className="max-w-7xl mx-auto px-6 py-3 items-center mt-9 grid md:grid-cols-2  gap-12">
+const Firstimp = () => {
+
+  const texts = [
+    "not just answers.",
+    "real understanding.",
+    "deep focus.",
+    "true clarity."
+  ];
+
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prev) => (prev + 1) % texts.length);
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const navigate = useNavigate();
+  const { isAuthenticated } = useAuth();
+
+  return (
+    <motion.div
+      variants={container}
+      initial="hidden"
+      animate="show"
+      className="min-h-screen bg-linear-to-r from-primary-dark to-primary text-white relative overflow-hidden"
+    >
+      <div className="max-w-7xl mx-auto px-6 py-3 items-center mt-9 grid md:grid-cols-2 gap-12">
 
         {/* LEFT */}
         <div>
-         <motion.h1 variants={heading} className="flex items-center text-9xl font-bold leading-tight mb-8">
-  Synaply 
-  
-</motion.h1>
-
-
-          <motion.h1 variants={item}  className="text-5xl font-bold leading-tight text-soft">
-       Built for focused learning - <br /> 
-
-<AnimatePresence mode="wait">
-        <motion.span
-          key={texts[index]}
-          initial={{ opacity: 0, y: 10 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: -10 }}
-          transition={{ duration: 0.5 }}
-        >
-          {texts[index]}
-        </motion.span>
-      </AnimatePresence>
-
-
-
+          <motion.h1
+            variants={heading}
+            className="flex items-center text-9xl font-bold leading-tight mb-8"
+          >
+            Synaply
           </motion.h1>
 
+          <motion.h1
+            variants={item}
+            className="text-5xl font-bold leading-tight text-soft"
+          >
+            Built for focused learning - <br />
 
+            <AnimatePresence mode="wait">
+              <motion.span
+                key={texts[index]}
+                initial={{ opacity: 0, y: 10 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -10 }}
+                transition={{ duration: 0.5 }}
+              >
+                {texts[index]}
+              </motion.span>
+            </AnimatePresence>
+          </motion.h1>
 
           <motion.p variants={item} className="mt-6 text-gray-400 text-lg">
-           Organize, revise, and test yourself with tools designed to help you actually retain what you learn.
+            Organize, revise, and test yourself with tools designed to help you actually retain what you learn.
           </motion.p>
 
-         <motion.button
-  variants={buttonAnim}
-  onClick={() => navigate(isAuthenticated ? "/dashboard" : "/login")}
-  className="mt-8 bg-white text-primary-dark px-6 py-3 rounded-full font-semibold transition"
->
-  {isAuthenticated ? "Go to Dashboard" : "Get Started"}
-</motion.button>
+          <motion.button
+            variants={buttonAnim}
+            onClick={() => navigate(isAuthenticated ? "/dashboard" : "/login")}
+            className="mt-8 bg-white text-primary-dark px-6 py-3 rounded-full font-semibold transition"
+          >
+            {isAuthenticated ? "Go to Dashboard" : "Get Started"}
+          </motion.button>
         </div>
 
-
+        {/* RIGHT */}
         <div>
-
-<motion.img variants={imageAnim} src={examboy} />
+          <motion.img variants={imageAnim} src={examboy} />
         </div>
 
-       
-  
       </div>
-
-     
-
-   
     </motion.div>
-  )
-}
+  );
+};
 
-export default Firstimp
+export default Firstimp;
